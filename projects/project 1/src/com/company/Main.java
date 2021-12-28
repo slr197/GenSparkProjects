2d113ac76381505725e72e96f1fed34720a6a591
Shania Richardson <slrichardson444@gmail.com>
2021-12-28 15:12:20 -0600

    Projects are uploaded here.

---
+++ projects/project 1/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+    public class Main {
+        public static void main(String[] args) {
+            int time = 1;
+            if (time < 2) {
+                System.out.println("You ran into the bad dragon! OH NO!");
+            } else if (time > 1) {
+                System.out.println("You caught the sleeping dragon");
+            }  else {
+                System.out.println("You dodged a bullet man. Phew!");
+            }
+        }
+    }
